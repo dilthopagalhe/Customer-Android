1e09494b8aab155bab97b97b9c22207733129ca9
Dilshan-Madhushanka <46591533+teddiya@users.noreply.github.com>
2020-10-24 01:31:38 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/hacktoberfest/MainActivity.java
+++ app/src/main/java/com/example/hacktoberfest/MainActivity.java
@@ -5,10 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    EditText editText1;
+    EditText editText2;
+    EditText editText3;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        editText1=(EditText)findViewById(R.id.username);
+        editText2=(EditText)findViewById(R.id.email);
+        editText3=(EditText)findViewById(R.id.password);
+        button=(Button) findViewById(R.id.login);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editText1.getText().toString().equals("Shan Athukoreala") &&
+                        editText2.getText().toString().equals("Shan@gmail.com") &&
+                        editText3.getText().toString().equals("Shan1@") ){
+
+                }
+            }
+        });
     }
 }
